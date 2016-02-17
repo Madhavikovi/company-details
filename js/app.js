a3686da28c14c9d125340e065acd6de2076de205
Madhavi Kovi <mkovi@altimetrik.com>
2016-02-17 11:55:49 +0530

    updated readme and chnages at add.js

--- js/app.js
+++ js/app.js
@@ -3,7 +3,6 @@
 angular.module('myApp', [])
   .controller('CompanyController', function($scope, $http){
 	  $scope.getCompanyDetails = function(){
-
 		  var primarySymbol = '';
 		  $http.get("http://edgaronline.api.mashery.com/v2/companies?fields=primarysymbol&companynames=" + $scope.company + "&appkey=apfn5drrntkur4gk4ebkdggj")
 		 	 .then(function(response){
@@ -21,47 +20,49 @@ angular.module('myApp', [])
 				 			}
 			 			 });
 			 		 });
-		 		 }
-		 		 if (primarySymbol == '')
-		 			primarySymbol = 'MSFT';
-		 		 
-		 		$http.get("http://edgaronline.api.mashery.com/v2/corefinancials/ann?primarysymbols=" + primarySymbol + "&appkey=apfn5drrntkur4gk4ebkdggj")
-		    	 .then(function(response){ 
-		    		 data = response.data.result.rows;
-		    		 data.forEach(function(key, index){
-				 		 rowData = key.values;
-				 		 var changedRowData = {};
-			 			 rowData.forEach(function(key, index){
-			 				changedRowData[key.field] = key.value;
-			 			 });
-		    			 key.values = changedRowData;
-			 		 });
-		    		 
-		    		 var itemHash = {};
-		    		 data.forEach(function(key, index){
-				 		 rowData = key.values;
+				 		$http.get("http://edgaronline.api.mashery.com/v2/corefinancials/ann?primarysymbols=" + primarySymbol + "&appkey=apfn5drrntkur4gk4ebkdggj")
+				    	 .then(function(response){ 
+				    		 data = response.data.result.rows;
+				    		 data.forEach(function(key, index){
+						 		 rowData = key.values;
+						 		 var changedRowData = {};
+					 			 rowData.forEach(function(key, index){
+					 				changedRowData[key.field] = key.value;
+					 			 });
+				    			 key.values = changedRowData;
+					 		 });
+				    		 
+				    		 var itemHash = {};
+				    		 data.forEach(function(key, index){
+						 		 rowData = key.values;
+						 		 
+						 		 $.each(rowData, function(key, value){
+					 				 if ($.isEmptyObject(itemHash[rowData.companyname]))
+					 					 itemHash[rowData.companyname] = {};
+					 				 if ($.isEmptyObject(itemHash[rowData.companyname][key]))
+					 					 itemHash[rowData.companyname][key] = {};
+					 				 
+					 				 if (angular.isNumber(value))
+					 					 value = (value + "").replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+					 				 
+					 				 itemHash[rowData.companyname][key][rowData.fiscalyear] = value;
+					 			 });
+					 		 });
+				    		 $scope.coreFinanceAnn = itemHash;
+				    		 $scope.error = ''; })
+				    	 .catch(function(response){
+				    		 $scope.error = response.data.Message;
+				         });
 				 		 
-				 		 $.each(rowData, function(key, value){
-			 				 if ($.isEmptyObject(itemHash[rowData.companyname]))
-			 					 itemHash[rowData.companyname] = {};
-			 				 if ($.isEmptyObject(itemHash[rowData.companyname][key]))
-			 					 itemHash[rowData.companyname][key] = {};
-			 				 
-			 				 if (angular.isNumber(value))
-			 					 value = (value + "").replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
-			 				 
-			 				 itemHash[rowData.companyname][key][rowData.fiscalyear] = value;
-			 			 });
-			 		 });
-		    		 $scope.coreFinanceAnn = itemHash;
-		    		 $scope.error = ''; })
+				 		$scope.error = '';
+				 	 }
+			 		 else{
+			 			$scope.error = 'No details are found';
+			 			$scope.coreFinanceAnn = '';
+			 		 }
+		 		  })
 		    	 .catch(function(response){
 		    		 $scope.error = response.data.Message;
-		         });
-		 		
-		 		
-		 		 
-		 	 })
-		 	 
+		         }); 
 	  }
   });
